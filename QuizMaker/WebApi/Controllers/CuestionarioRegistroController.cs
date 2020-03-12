4d1ab4479425b1ce2aa79b5fea690b6c3385a818
Manuel Padilla <52419116+SOLFAHSA@users.noreply.github.com>
2020-03-12 00:24:48 -0600

    WebApi

---
+++ QuizMaker/WebApi/Controllers/CuestionarioRegistroController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Domain.Business;
+using Microsoft.AspNetCore.Mvc;
+using Services.Business;
+using Unity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/ [action]/{id?}")]
+    public class CuestionarioRegistroController : BaseAPIController
+    {
+        #region Field
+        [Dependency]
+        public ICuestionarioRegistroServices cuestionarioregistroServices { get; set; }
+        #endregion
+
+        public IEnumerable<CuestionarioRegistro> getAll()
+        {
+            try
+            {
+                var data = cuestionarioregistroServices.GetAll();
+                return data;
+            }
+            catch  (System.Exception ex)
+            {
+                return null;
+                throw;
+            }  
+        }
+
+    }
+}
+
