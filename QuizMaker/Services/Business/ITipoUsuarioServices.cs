e26329fe32f1e91baad1c19b0df8b672ee1f37aa
DALR18 <60826660+DALR18@users.noreply.github.com>
2020-03-19 16:47:59 -0600

    Servicios TipoUsuario
    Servicios TipoUsuario

---
+++ QuizMaker/Services/Business/ITipoUsuarioServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Bussines;
+
+namespace Services.Bussines
+{
+    public interface ITipoUsuarioServices
+    {
+        public bool save(TipoUsuario tu);
+
+        public TipoUsuario GetbyId(int id);
+
+        public bool Delete(int id);
+
+        public bool Update(TipoUsuario tu);
+
+        public IEnumerable<TipoUsuario> GetAll();
+
+        public bool Exist(string valor);
+    }
+}
---
+++ QuizMaker/Services/Business/TipoUsuarioServices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using Domain.Bussines;
+using Data.Repositories;
+
+namespace Services.Bussines
+{
+    public class TipoUsuarioServices : ITipoUsuarioServices
+    {
+        #region FIELDS
+        [Dependency]
+        public ITipoUsuarioRepository tipoUsuarioRepository { get; set; }
+        #endregion
+        public bool Delete(int id)
+        {
+            return tipoUsuarioRepository.Delete(id);
+        }
+
+        public bool Exist(string valor)
+        {
+            return tipoUsuarioRepository.Exist(valor);
+        }
+
+        public IEnumerable<TipoUsuario> GetAll()
+        {
+            return tipoUsuarioRepository.GetAll();
+        }
+
+        public TipoUsuario GetbyId(int id)
+        {
+            return tipoUsuarioRepository.GetbyId(id);
+        }
+
+        public bool save(TipoUsuario tu)
+        {
+            return tipoUsuarioRepository.save(tu);
+        }
+
+        public bool Update(TipoUsuario tu)
+        {
+            return tipoUsuarioRepository.Update(tu);
+        }
+    }
+}
