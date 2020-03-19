21a19266860545605b2db410d09a05aea7d3d1e0
DALR18 <60826660+DALR18@users.noreply.github.com>
2020-03-19 16:47:19 -0600

    Dominio TipoUsuario
    Dominio TipoUsuario

---
+++ QuizMaker/Dominio/Business/TipoUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Bussines
+{
+    public class TipoUsuario
+    {
+        public int Id { get; set; }
+        public string Descriptor { get; set; } 
+    }
+}
