c2dd98e5bba2530f17e8afb6464c78bcd696469f
DALR18 <60826660+DALR18@users.noreply.github.com>
2020-03-19 16:48:12 -0600

    Servicios Usuario
    Servicios Usuario

---
+++ QuizMaker/Services/Business/IUsuarioServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Bussines;
+
+namespace Services.Bussines
+{
+    public interface IUsuarioServices
+    {
+        public bool save(Usuario u);
+
+        public Usuario GetbyId(int id);
+
+        public bool Delete(int id);
+
+        public bool Update(Usuario u);
+
+        public IEnumerable<Usuario> GetAll();
+
+        public bool Exist(string valor);
+    }
+}
---
+++ QuizMaker/Services/Business/UsuarioServices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using Domain.Bussines;
+using Data.Repositories;
+
+namespace Services.Bussines
+{
+    class UsuarioServices : IUsuarioServices
+    {
+        #region FIELDS
+        [Dependency]
+        public IUsuarioRepository usuarioRepository { get; set; }
+        #endregion
+        public bool Delete(int id)
+        {
+            return usuarioRepository.Delete(id);
+        }
+
+        public bool Exist(string valor)
+        {
+            return usuarioRepository.Exist(valor);
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            return usuarioRepository.GetAll();
+        }
+
+        public Usuario GetbyId(int id)
+        {
+            return usuarioRepository.GetbyId(id);
+        }
+
+        public bool save(Usuario u)
+        {
+            return usuarioRepository.save(u);
+        }
+
+        public bool Update(Usuario u)
+        {
+            return usuarioRepository.Update(u);
+        }
+    }
+}
